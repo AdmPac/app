6e01703a5335a2e591604d568155274ae531d632
Andrew <for_apb2015@mail.ru>
2025-10-29 16:14:09 +0300

    Первичная страница заказов

---
+++ resources/views/market/index.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <nav>
+        <ul>
+            <li><a href="/">Главная</a></li>
+            <li><a href="orders">Корзина</a></li>
+        </ul>
+        
+        <main>
+            <div style="display:flex; flex-direction:row; flex-wrap:wrap;">
+                <div style="display: flex; flex-direction:column; height: 300px; width: 300px; border: 1px solid black; margin: 10px">
+                    <img src="" alt="picture" style="width:100%; height: 20vh; border: 1px solid red">
+                    <span id="name" style="padding:0px">Название:</span>
+                    <span id="description" style="padding:0px">Описание:</span>
+                </div>
+            </div>
+        </main>
+        
+        </div>
+    </nav>
+</div>
