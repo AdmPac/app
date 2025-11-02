465742ce821581eb9aa04f2e4072f1cc90879996
Andrew <for_apb2015@mail.ru>
2025-11-02 18:01:12 +0300

    Добавил маршруты

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', [ProductController::class, 'index']);
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/{id}', [ProductController::class, 'edit'])->name('product.page.edit');
+Route::put('/product/{id}', [ProductController::class, 'store'])->name('product.edit');
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
 
