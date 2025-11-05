a9c2ecf580d07c7c8f370a84b8c9dcc4ddbb43cc
Andrew <for_apb2015@mail.ru>
2025-11-05 16:49:54 +0300

    Перенес проверку статуса в запрос

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,15 +2,13 @@
 <main>
     <div style="display:flex; flex-direction:row; flex-wrap:wrap;">
         @foreach ($products as $product)
-            @if ($product->status->id == 1)
-                <x-card.product.preview 
-                    :name="$product->name"
-                    :description="$product->description"
-                    :img="$product->img"
-                    :cost="$product->cost"
-                    :limit="$product->limit"
-                />
-            @endif
+            <x-card.product.preview 
+                :name="$product->name"
+                :description="$product->description"
+                :img="$product->img"
+                :cost="$product->cost"
+                :limit="$product->limit"
+            />
         @endforeach
     </div>
 </main>
