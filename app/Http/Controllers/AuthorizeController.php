1c672c2608faa29afa88ad57440c7905b50f8bf7
Andrew <for_apb2015@mail.ru>
2025-11-05 15:57:06 +0300

    Реализация авторизации с проверкой доступов

---
+++ app/Http/Controllers/AuthorizeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorizeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function authorize()
+    {
+        if (Auth::user()) return redirect()->route('product.index');
+        return view('authorize');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function check(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if(Auth::attempt($loginData)) {
+            return back();
+        }
+        return back()->withErrors(['email' => "Неверные данные"])->onlyInput();
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthorizeController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
@@ -12,4 +13,8 @@ Route::get('/product/{id}', [ProductController::class, 'edit'])->name('product.p
 Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update')->whereNumber('id');
 Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.delete')->whereNumber('id');
 Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+Route::get('/authorize', [AuthorizeController::class, 'authorize'])->name('authorize');
+Route::post('/authorize', [AuthorizeController::class, 'check'])->name('authorize.check');
+Route::post('/authorize/logout/', [AuthorizeController::class, 'logout'])->name('authorize.logout');
 
