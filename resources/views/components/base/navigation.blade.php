a67f1fd43858e3db1280cb3f771154ddd656d9a7
Andrew <for_apb2015@mail.ru>
2025-11-07 12:06:48 +0300

    Добавил именованные ссылки

--- resources/views/components/base/navigation.blade.php
+++ resources/views/components/base/navigation.blade.php
@@ -1,15 +1,15 @@
 <nav>
     <ul>
-        <li><a href="{{url('/')}}"><b>Главная</b></a></li>
+        <li><a href="{{route('product.index')}}"><b>Главная</b></a></li>
         <li><a href="{{url('orders')}}">Корзина</a></li>
         @if(auth()->check() && Auth::user()->is_admin)
-            <li><a href="{{url('admin')}}">Админка</a></li>
+            <li><a href="{{route('admin.index')}}">Админка</a></li>
         @endif
         @if(auth()->check())
-            <li><a href="{{url('authorize')}}">Профиль [{{Auth::user()->name}}]</a></li>
+            <li>Профиль [{{Auth::user()->name}}]</li>
             <li><a href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Выход</a></li>
         @else
-            <li><a href="{{url('authorize')}}">Авторизация</a></li>
+            <li><a href="{{url('login')}}">Авторизация</a></li>
         @endif
     </ul>
 </nav>
