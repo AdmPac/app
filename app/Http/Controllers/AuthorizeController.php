f25f1224956ff01993e7990a4a9188a02522c110
Andrew <for_apb2015@mail.ru>
2025-11-10 18:12:37 +0300

    api авторизации

--- app/Http/Controllers/AuthorizeController.php
+++ app/Http/Controllers/AuthorizeController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Order;
-use App\Models\OrderItems;
-use App\Models\Product;
-use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,56 +11,33 @@ class AuthorizeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function login()
-    {
-        if (Auth::user()) return redirect()->route('product.index');
-        return view('authorize');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function check(Request $request)
+    public function login(Request $request)
     {
         $loginData = $request->validate([
-            'email' => 'required|email',
+            'email' => 'required|email' ,
             'password' => 'required|string',
         ]);
 
-        if(Auth::attempt($loginData)) {
-            
-            $uid = Auth::user()->id;
-            $orderModel = Order::firstOrCreate([
-                'user_id' => $uid,
-                'status_id' => 1,
-            ]);
-            
-            if ($orderModel->product()->first() === null) {
-                $orderSession = session('order');
-                $products = Product::whereIn('id', array_keys($orderSession))->get()->toArray();
-                $products = array_column($products, null, 'id');
-                
-                foreach ($orderSession as $productId => $quantity) {
-                    // dump($products, $productId);
-                    OrderItems::create([
-                        'order_id' => $orderModel->id,
-                        'product_id' => $productId,
-                        'name' => $products[$productId]['name'],
-                        'img' => $products[$productId]['img'],
-                        'description' => $products[$productId]['description'],
-                        'cost' => $products[$productId]['cost'],
-                        'quantity' => $quantity,
-                    ]);
-                }
-            }
-            return back();
+        if($user = Auth::attempt($loginData)) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'message' => 'Login success',
+                'token' => $token,
+            ], 200);
         }
-        return back()->withErrors(['email' => "Неверные данные"])->onlyInput();
+
+        throw ValidationException::withMessages([
+            'email' => [__('auth.failed')],
+        ]);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        Auth::logout();
-        return back();
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'logout success',
+        ]);
     }
 }
