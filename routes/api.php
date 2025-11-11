050afd23c87b54c2a83256b2a755ca26d045d480
Andrew <for_apb2015@mail.ru>
2025-11-11 19:01:57 +0300

    Добавил работу с jwt

--- routes/api.php
+++ routes/api.php
@@ -4,36 +4,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Session\Middleware\StartSession;
 
-use App\Http\Controllers\AuthorizeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\AuthController;
 
 
-Route::post('/login', [AuthorizeController::class, 'login']); // Вход
 Route::get('/products/{id}', [ProductController::class, 'get']); // Конкретный товар
 Route::get('/products', [ProductController::class, 'getAll']); // Все товары
 
 Route::middleware([
-    StartSession::class
+    StartSession::class,
 ])->group(function() {
     Route::get('/cart', [CartController::class, 'get']); // Корзина
     Route::post('/cart/{id}', [CartController::class, 'post'])->whereNumber('id'); // Добавление товара в корзину
-    Route::patch('/cart/{id}', [CartController::class, 'patch'])->whereNumber('id')->middleware('auth:sanctum'); // Изменение кол-ва товара в корзине
+    Route::patch('/cart/{id}', [CartController::class, 'patch'])->whereNumber('id')->middleware('jwt.auth'); // Изменение кол-ва товара в корзине
     Route::delete('/cart/{id}', [CartController::class, 'delete'])->whereNumber('id'); // Удаление товара из корзины
 });
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
- 
-    Route::post('/logout', [AuthorizeController::class, 'logout']); // Выход
+Route::middleware('jwt.auth')->group(function() {
     Route::get('/orders', [OrderController::class, 'get']); // Заказы текущего юзера
     Route::post('/cart/delivery', [CartController::class, 'delivery']); // Оформление доставки
 });
 
-Route::middleware(['auth:sanctum', 'can:access-admin'])->group(function() {
+Route::middleware(['jwt.auth', 'can:access-admin'])->group(function() {
     Route::delete('/products/{id}', [ProductController::class, 'delete']);
     Route::put('/products/{id}', [ProductController::class, 'put']); // Измененеие товара - полное
     Route::patch('/products/{id}', [ProductController::class, 'patch']); // Изменение товара - частичное
@@ -41,4 +35,13 @@ Route::middleware(['auth:sanctum', 'can:access-admin'])->group(function() {
     
     Route::get('/orders/{uid}', [OrderController::class, 'getByuID']); // заказы определенного юзера
     Route::patch('/orders/{id}', [OrderController::class, 'patch']); // заказы определенного юзера
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
