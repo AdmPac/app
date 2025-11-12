d36fdd079c74d18053d82a2f4639e65be2c7d78c
Andrew <for_apb2015@mail.ru>
2025-11-12 18:10:37 +0300

    Убрал web

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AuthorizeController;
-use App\Http\Controllers\OrderController;
-
-// Route::middleware(['auth', 'can:access-admin'])->group(function() {
-//     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-//     Route::patch('/orders/{id}/edit', [OrderController::class, 'edit'])->name('order.form.edit');
-// });
-
-// Route::middleware('auth')->group(function() {
-//     Route::get('/orders/{id}', [OrderController::class, 'form'])->name('order.form');
-//     Route::post('/orders/{id}/delivery', [OrderController::class, 'delivery'])->name('order.delivery');
-// });
